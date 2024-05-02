14c1409ca00e62956c486feb11c7159636bf1973
sivakami123 <sivakamip398@gmail.com>
2024-05-02 11:36:24 +0530

    news classification

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import joblib
+
+model=joblib.load('news-classification-model.pkl')
+news_labels={'0':'Technical','1':'Business','2':'Sports','3':'Entertainment','4':'Politics'}
+
+st.title("News classification")
+
+user_input=st.text_area('Enter news here:')
+if st.button("predict"):
+    predicted_label=model.predict([user_input])[0]
+    predicted_news_label=news_labels[str(predicted_label)]
+
+    st.info(f"text class is: {predicted_news_label}")
